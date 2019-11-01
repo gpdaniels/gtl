1f1a30bcb068915b72aed421b48b693da23657c2
Geoffrey Daniels <geoff@gpdaniels.com>
2019-11-01 13:43:47 +0000

    Adding placeholder ideas for hash classes.

---
+++ Tests/hash/sha2.test.cpp
@@ -0,0 +1,53 @@
+/*
+The MIT License
+Copyright (c) 2019 Geoffrey Daniels. http://gpdaniels.com/
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE
+*/
+
+#include <main.tests.hpp>
+#include <benchmark.tests.hpp>
+#include <require.tests.hpp>
+
+#include <hash/sha2>
+
+#if defined(_MSC_VER)
+#   pragma warning(push, 0)
+#endif
+
+#include <type_traits>
+
+#if defined(_MSC_VER)
+#   pragma warning(pop)
+#endif
+
+TEST(sha2, traits, standard) {
+    REQUIRE(sizeof(gtl::sha2) >= 0);
+
+    REQUIRE((std::is_pod<gtl::sha2>::value == true));
+
+    REQUIRE((std::is_trivial<gtl::sha2>::value == true));
+
+    REQUIRE((std::is_trivially_copyable<gtl::sha2>::value == true));
+
+    REQUIRE((std::is_standard_layout<gtl::sha2>::value == true));
+}
+
+TEST(sha2, constructor, empty) {
+    gtl::sha2 sha2;
+    do_not_optimise_away(sha2);
+}
+
