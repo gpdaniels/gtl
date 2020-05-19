66bd63fa1ffd4c6819d608365020e70eb31b85a8
Geoffrey Daniels <geoff@gpdaniels.com>
2020-05-19 10:26:57 +0100

    Add a quiet flag argument to the testbench main.

--- Testbench/main.tests.cpp
+++ Testbench/main.tests.cpp
@@ -32,23 +32,7 @@ int main(int argument_count, char* arguments[]) {
     setvbuf(stdout, nullptr, _IONBF, 0);
     setvbuf(stderr, nullptr, _IONBF, 0);
 
-    // Notifiy if any sanitizers are running.
-    #if GTL_HAS_SANITIZER_ADDRESS
-        PRINT("Testing under address sanitizer.\n");
-    #endif
-
-    #if GTL_HAS_SANITIZER_MEMORY
-        PRINT("Testing under memory sanitizer.\n");
-    #endif
-
-    #if GTL_HAS_SANITIZER_THREAD
-        PRINT("Testing under thread sanitizer.\n");
-    #endif
-
-    #if GTL_HAS_SANITIZER_UNDEFINED_BEHAVIOR
-        PRINT("Testing under undefined behavior sanitizer.\n");
-    #endif
-
+    bool quiet = false;
     const char* filter_file = nullptr;
     const char* filter_group = nullptr;
     const char* filter_name = nullptr;
@@ -58,11 +42,21 @@ int main(int argument_count, char* arguments[]) {
     for (int argument_index = 0; argument_index < argument_count; ++argument_index) {
         const int arguments_remaining = argument_count - argument_index;
 
+        // Extract a quiet flag from the command line arguments.
+        if (arguments_remaining >= 1) {
+            if (testbench::is_string_same("-q", arguments[argument_index]) || testbench::is_string_same("--quiet", arguments[argument_index])) {
+                quiet = true;
+                continue;
+            }
+        }
+
         // Extract a test group filter from the command line arguments.
         if (arguments_remaining >= 2) {
             if (testbench::is_string_same("-f", arguments[argument_index]) || testbench::is_string_same("--file", arguments[argument_index])) {
                 filter_file = arguments[++argument_index];
-                PRINT("Filtering tests by file: %s\n", filter_file);
+                if (!quiet) {
+                    PRINT("Filtering tests by file: %s\n", filter_file);
+                }
                 continue;
             }
         }
@@ -71,7 +65,9 @@ int main(int argument_count, char* arguments[]) {
         if (arguments_remaining >= 2) {
             if (testbench::is_string_same("-g", arguments[argument_index]) || testbench::is_string_same("--group", arguments[argument_index])) {
                 filter_group = arguments[++argument_index];
-                PRINT("Filtering tests by group: %s\n", filter_group);
+                if (!quiet) {
+                    PRINT("Filtering tests by group: %s\n", filter_group);
+                }
                 continue;
             }
         }
@@ -80,7 +76,9 @@ int main(int argument_count, char* arguments[]) {
         if (arguments_remaining >= 2) {
             if (testbench::is_string_same("-n", arguments[argument_index]) || testbench::is_string_same("--name", arguments[argument_index])) {
                 filter_name = arguments[++argument_index];
-                PRINT("Filtering tests by name: %s\n", filter_name);
+                if (!quiet) {
+                    PRINT("Filtering tests by name: %s\n", filter_name);
+                }
                 continue;
             }
         }
@@ -89,13 +87,42 @@ int main(int argument_count, char* arguments[]) {
         if (arguments_remaining >= 1) {
             if (testbench::is_string_same("-x", arguments[argument_index]) || testbench::is_string_same("--execute", arguments[argument_index])) {
                 execute_in_process = false;
-                PRINT("Running tests in this process.\n");
+                if (!quiet) {
+                    PRINT("Running tests in this process.\n");
+                }
                 continue;
             }
         }
     }
+    
+    // Notifiy if any sanitizers are running.
+    #if GTL_HAS_SANITIZER_ADDRESS
+        if (!quiet) {
+            PRINT("Testing under address sanitizer.\n");
+        }
+    #endif
 
-    PRINT("Starting tests...\n");
+    #if GTL_HAS_SANITIZER_MEMORY
+        if (!quiet) {
+            PRINT("Testing under memory sanitizer.\n");
+        }
+    #endif
+
+    #if GTL_HAS_SANITIZER_THREAD
+        if (!quiet) {
+            PRINT("Testing under thread sanitizer.\n");
+        }
+    #endif
+
+    #if GTL_HAS_SANITIZER_UNDEFINED_BEHAVIOR
+        if (!quiet) {
+            PRINT("Testing under undefined behavior sanitizer.\n");
+        }
+    #endif
+
+    if (!quiet) {
+        PRINT("Starting tests...\n");
+    }
     
     unsigned long long TEST_COUNT = 0;
     unsigned long long TEST_FAILURE_COUNT = 0;
@@ -114,7 +141,7 @@ int main(int argument_count, char* arguments[]) {
         PRINT("Running [File: %s] [Group: %s] [Test: %s]...\n", current_test->get_file(), current_test->get_group(), current_test->get_name());
         ++TEST_COUNT;
         if (execute_in_process) {
-            const char* test_arguments[] = { "--execute", "--file", current_test->get_file(), "--group", current_test->get_group(), "--name", current_test->get_name(), nullptr };
+            const char* test_arguments[] = { "--quiet", "--execute", "--file", current_test->get_file(), "--group", current_test->get_group(), "--name", current_test->get_name(), nullptr };
             int result = testbench::launch_process(testbench::get_executable(), test_arguments);
             if (result != 0) {
                 ++TEST_FAILURE_COUNT;
@@ -130,7 +157,9 @@ int main(int argument_count, char* arguments[]) {
         PRINT("Finished [File: %s] [Group: %s] [Test: %s].\n", current_test->get_file(), current_test->get_group(), current_test->get_name());
     }
 
-    PRINT("Finished '%lld' tests. Detected '%lld' failures.\n", TEST_COUNT, TEST_FAILURE_COUNT);
+    if (!quiet) {
+        PRINT("Finished '%lld' tests. Detected '%lld' failures.\n", TEST_COUNT, TEST_FAILURE_COUNT);
+    }
 
     return (TEST_FAILURE_COUNT != 0);
 }
