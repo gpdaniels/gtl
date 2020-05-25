d88e19b5f24c53753643ed9d7128aaa786aa9758
Geoffrey Daniels <geoff@gpdaniels.com>
2020-05-25 09:27:21 +0100

    Adding ann and hmm classes

--- Tests/ai/ann.test.cpp
+++ Tests/ai/ann.test.cpp
@@ -20,6 +20,7 @@ THE SOFTWARE
 
 #include <main.tests.hpp>
 #include <benchmark.tests.hpp>
+#include <comparison.tests.hpp>
 #include <require.tests.hpp>
 
 #include <ai/ann>
@@ -35,17 +36,285 @@ THE SOFTWARE
 #endif
 
 TEST(ann, traits, standard) {
-    REQUIRE((std::is_pod<gtl::ann>::value == true));
+    REQUIRE((std::is_pod<gtl::ann<float>>::value == false));
 
-    REQUIRE((std::is_trivial<gtl::ann>::value == true));
+    REQUIRE((std::is_trivial<gtl::ann<float>>::value == false));
 
-    REQUIRE((std::is_trivially_copyable<gtl::ann>::value == true));
+    REQUIRE((std::is_trivially_copyable<gtl::ann<float>>::value == false));
 
-    REQUIRE((std::is_standard_layout<gtl::ann>::value == true));
+    REQUIRE((std::is_standard_layout<gtl::ann<float>>::value == true));
 }
 
 TEST(ann, constructor, empty) {
-    gtl::ann ann;
+    gtl::ann<float> ann;
     testbench::do_not_optimise_away(ann);
 }
 
+TEST(ann, constructor, minimal_arguments) {
+    gtl::ann<float> ann({0, 0});
+    testbench::do_not_optimise_away(ann);
+}
+
+TEST(ann, constructor, maximal_arguments) {
+    gtl::ann<float> ann({0, 0}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::sigmoid);
+    testbench::do_not_optimise_away(ann);
+}
+
+TEST(ann, function, train) {
+    gtl::ann<float> ann({0, 0});
+    ann.train({}, {});
+}
+
+TEST(ann, function, process) {
+    gtl::ann<float> ann({0, 0});
+    std::vector<float> output = ann.process({});
+}
+
+TEST(ann, evaluate, or_gate) {
+    gtl::ann<float> ann = gtl::ann<float>({2, 5, 1});
+
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {0.0}});
+    data.push_back({{0.0, 1.0}, {1.0}});
+    data.push_back({{1.0, 0.0}, {1.0}});
+    data.push_back({{1.0, 1.0}, {1.0}});
+
+    for (unsigned int iterations = 0; iterations < 4 * 10000; ++iterations) {
+        ann.train(data[iterations % 4].first, data[iterations % 4].second);
+    }
+
+    std::vector<float> output;
+    output = ann.process(data[0].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[0].second[0], 1e-6f), "0 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[0].second[0]));
+
+    output = ann.process(data[1].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[1].second[0], 1e-6f), "0 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[1].second[0]));
+
+    output = ann.process(data[2].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[2].second[0], 1e-6f), "1 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[2].second[0]));
+
+    output = ann.process(data[3].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[3].second[0], 1e-6f), "1 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[3].second[0]));
+}
+
+TEST(ann, evaluate, and_gate) {
+    gtl::ann<float> ann = gtl::ann<float>({2, 5, 1});
+
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {0.0}});
+    data.push_back({{0.0, 1.0}, {0.0}});
+    data.push_back({{1.0, 0.0}, {0.0}});
+    data.push_back({{1.0, 1.0}, {1.0}});
+
+    for (unsigned int iterations = 0; iterations < 4 * 10000; ++iterations) {
+        ann.train(data[iterations % 4].first, data[iterations % 4].second);
+    }
+
+    std::vector<float> output;
+    output = ann.process(data[0].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[0].second[0], 1e-6f), "0 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[0].second[0]));
+
+    output = ann.process(data[1].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[1].second[0], 1e-6f), "0 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[1].second[0]));
+
+    output = ann.process(data[2].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[2].second[0], 1e-6f), "1 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[2].second[0]));
+
+    output = ann.process(data[3].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[3].second[0], 1e-6f), "1 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[3].second[0]));
+}
+
+TEST(ann, evaluate, nor_gate) {
+    gtl::ann<float> ann = gtl::ann<float>({2, 5, 1});
+
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {1.0}});
+    data.push_back({{0.0, 1.0}, {0.0}});
+    data.push_back({{1.0, 0.0}, {0.0}});
+    data.push_back({{1.0, 1.0}, {0.0}});
+
+    for (unsigned int iterations = 0; iterations < 4 * 10000; ++iterations) {
+        ann.train(data[iterations % 4].first, data[iterations % 4].second);
+    }
+
+    std::vector<float> output;
+    output = ann.process(data[0].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[0].second[0], 1e-6f), "0 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[0].second[0]));
+
+    output = ann.process(data[1].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[1].second[0], 1e-6f), "0 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[1].second[0]));
+
+    output = ann.process(data[2].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[2].second[0], 1e-6f), "1 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[2].second[0]));
+
+    output = ann.process(data[3].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[3].second[0], 1e-6f), "1 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[3].second[0]));
+}
+
+TEST(ann, evaluate, nand_gate) {
+    gtl::ann<float> ann = gtl::ann<float>({2, 5, 1});
+
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {1.0}});
+    data.push_back({{0.0, 1.0}, {1.0}});
+    data.push_back({{1.0, 0.0}, {1.0}});
+    data.push_back({{1.0, 1.0}, {0.0}});
+
+    for (unsigned int iterations = 0; iterations < 4 * 10000; ++iterations) {
+        ann.train(data[iterations % 4].first, data[iterations % 4].second);
+    }
+
+    std::vector<float> output;
+    output = ann.process(data[0].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[0].second[0], 1e-6f), "0 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[0].second[0]));
+
+    output = ann.process(data[1].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[1].second[0], 1e-6f), "0 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[1].second[0]));
+
+    output = ann.process(data[2].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[2].second[0], 1e-6f), "1 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[2].second[0]));
+
+    output = ann.process(data[3].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[3].second[0], 1e-6f), "1 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[3].second[0]));
+}
+
+TEST(ann, evaluate, xor_gate) {
+    gtl::ann<float> ann = gtl::ann<float>({2, 5, 1});
+
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {0.0}});
+    data.push_back({{0.0, 1.0}, {1.0}});
+    data.push_back({{1.0, 0.0}, {1.0}});
+    data.push_back({{1.0, 1.0}, {0.0}});
+
+    for (unsigned int iterations = 0; iterations < 4 * 10000; ++iterations) {
+        ann.train(data[iterations % 4].first, data[iterations % 4].second);
+    }
+
+    std::vector<float> output;
+    output = ann.process(data[0].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[0].second[0], 1e-6f), "0 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[0].second[0]));
+
+    output = ann.process(data[1].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[1].second[0], 1e-6f), "0 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[1].second[0]));
+
+    output = ann.process(data[2].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[2].second[0], 1e-6f), "1 0 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[2].second[0]));
+
+    output = ann.process(data[3].first);
+    REQUIRE(testbench::is_value_approx(output[0], data[3].second[0], 1e-6f), "1 1 => %f != %f\n", static_cast<double>(output[0]), static_cast<double>(data[3].second[0]));
+}
+
+TEST(ann, evaluate, learning_rate) {
+    std::vector<std::pair<std::vector<float>, std::vector<float>>> data;
+    data.push_back({{0.0, 0.0}, {0.0}});
+    data.push_back({{0.0, 1.0}, {1.0}});
+    data.push_back({{1.0, 0.0}, {1.0}});
+    data.push_back({{1.0, 1.0}, {0.0}});
+
+    auto train_network = [&data](gtl::ann<float>& ann, unsigned int& iterations) -> bool {
+        bool learnt = true;
+        for (iterations = 0; iterations < 4 * 10000; ++iterations) {
+            ann.train(data[iterations % 4].first, data[iterations % 4].second);
+            learnt = true;
+            std::vector<float> output;
+            output = ann.process(data[0].first);
+            learnt &= testbench::is_value_approx(output[0], data[0].second[0], 1e-6f);
+            output = ann.process(data[1].first);
+            learnt &= testbench::is_value_approx(output[0], data[1].second[0], 1e-6f);
+            output = ann.process(data[2].first);
+            learnt &= testbench::is_value_approx(output[0], data[2].second[0], 1e-6f);
+            output = ann.process(data[3].first);
+            learnt &= testbench::is_value_approx(output[0], data[3].second[0], 1e-6f);
+            if (learnt) {
+                break;
+            }
+        }
+        return learnt;
+    };
+
+    unsigned int iterations_none     = 0;
+    unsigned int iterations_identity = 0;
+    unsigned int iterations_step     = 0;
+    unsigned int iterations_sigmoid  = 0;
+    unsigned int iterations_softplus = 0;
+    unsigned int iterations_tanh     = 0;
+    unsigned int iterations_erf      = 0;
+
+    gtl::ann<float> ann_none     = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::none);
+    gtl::ann<float> ann_identity = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::identity);
+    gtl::ann<float> ann_step     = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::step);
+    gtl::ann<float> ann_sigmoid  = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::sigmoid);
+    gtl::ann<float> ann_softplus = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::softplus);
+    gtl::ann<float> ann_tanh     = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::tanh);
+    gtl::ann<float> ann_erf      = gtl::ann<float>({2, 5, 1}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::erf);
+
+    bool learnt_none     = train_network(ann_none, iterations_none);
+    bool learnt_identity = train_network(ann_identity, iterations_identity);
+    bool learnt_step     = train_network(ann_identity, iterations_step);
+    bool learnt_sigmoid  = train_network(ann_sigmoid, iterations_sigmoid);
+    bool learnt_softplus = train_network(ann_softplus, iterations_softplus);
+    bool learnt_tanh     = train_network(ann_tanh, iterations_tanh);
+    bool learnt_erf      = train_network(ann_erf, iterations_erf);
+
+    PRINT("Evaluate learning speed: \n");
+    PRINT(" none     -> %d in %d\n", learnt_none, iterations_none);
+    PRINT(" identity -> %d in %d\n", learnt_identity, iterations_identity);
+    PRINT(" step     -> %d in %d\n", learnt_step, iterations_step);
+    PRINT(" sigmoid  -> %d in %d\n", learnt_sigmoid, iterations_sigmoid);
+    PRINT(" softplus -> %d in %d\n", learnt_softplus, iterations_softplus);
+    PRINT(" tanh     -> %d in %d\n", learnt_tanh, iterations_tanh);
+    PRINT(" erf      -> %d in %d\n", learnt_erf, iterations_erf);
+
+    // Require that the activation functions with sensible derivatives all succeed.
+    REQUIRE(learnt_sigmoid);
+    REQUIRE(learnt_softplus);
+    REQUIRE(learnt_tanh);
+    REQUIRE(learnt_erf);
+}
+
+TEST(ann, evaluate, adder) {
+    gtl::ann<float> ann = gtl::ann<float>({8, 32, 32, 4}, gtl::ann<float>::initialisation_type::random, gtl::ann<float>::activation_type::sigmoid);
+
+    for (unsigned int iterations = 0; iterations < 50000; ++iterations) {
+        char input_lhs = (iterations>>4)&0b00001111;
+        char input_rhs = (iterations>>0)&0b00001111;
+        char target_bits = input_lhs + input_rhs;
+
+        std::vector<float> input = {
+            static_cast<float>(input_lhs >> 3 & 1), static_cast<float>(input_lhs >> 2 & 1), static_cast<float>(input_lhs >> 1 & 1), static_cast<float>(input_lhs >> 0 & 1),
+            static_cast<float>(input_rhs >> 3 & 1), static_cast<float>(input_rhs >> 2 & 1), static_cast<float>(input_rhs >> 1 & 1), static_cast<float>(input_rhs >> 0 & 1)
+        };
+        std::vector<float> target = {
+            static_cast<float>(target_bits >> 3 & 1), static_cast<float>(target_bits >> 2 & 1), static_cast<float>(target_bits >> 1 & 1), static_cast<float>(target_bits >> 0 & 1)
+        };
+
+        ann.train(input, target);
+    }
+
+    int incorrect = 0;
+
+    for (unsigned int iterations = 0; iterations < 256; ++iterations) {
+
+        char input_lhs = (iterations>>4)&0b00001111;
+        char input_rhs = (iterations>>0)&0b00001111;
+        char target_bits = input_lhs + input_rhs;
+
+        std::vector<float> input = {
+            static_cast<float>(input_lhs >> 3 & 1), static_cast<float>(input_lhs >> 2 & 1), static_cast<float>(input_lhs >> 1 & 1), static_cast<float>(input_lhs >> 0 & 1),
+            static_cast<float>(input_rhs >> 3 & 1), static_cast<float>(input_rhs >> 2 & 1), static_cast<float>(input_rhs >> 1 & 1), static_cast<float>(input_rhs >> 0 & 1)
+        };
+        std::vector<float> target = {
+            static_cast<float>(target_bits >> 3 & 1), static_cast<float>(target_bits >> 2 & 1), static_cast<float>(target_bits >> 1 & 1), static_cast<float>(target_bits >> 0 & 1)
+        };
+
+        std::vector<float> output;
+        output = ann.process(input);
+
+        if ((target[0]>0.5f != output[0]>0.5f) || (target[1]>0.5f != output[1]>0.5f) || (target[2]>0.5f != output[2]>0.5f) || (target[3]>0.5f != output[3]>0.5f)) {
+            ++incorrect;
+        }
+    }
+
+    PRINT("Incorrect: %d\n", incorrect);
+}
--- Tests/ai/hmm.test.cpp
+++ Tests/ai/hmm.test.cpp
@@ -35,17 +35,143 @@ THE SOFTWARE
 #endif
 
 TEST(hmm, traits, standard) {
-    REQUIRE((std::is_pod<gtl::hmm>::value == true));
+    REQUIRE((std::is_pod<gtl::hmm<float>>::value == false));
 
-    REQUIRE((std::is_trivial<gtl::hmm>::value == true));
+    REQUIRE((std::is_trivial<gtl::hmm<float>>::value == false));
 
-    REQUIRE((std::is_trivially_copyable<gtl::hmm>::value == true));
+    REQUIRE((std::is_trivially_copyable<gtl::hmm<float>>::value == false));
 
-    REQUIRE((std::is_standard_layout<gtl::hmm>::value == true));
+    REQUIRE((std::is_standard_layout<gtl::hmm<float>>::value == true));
 }
 
 TEST(hmm, constructor, empty) {
-    gtl::hmm hmm;
+    gtl::hmm<float> hmm;
     testbench::do_not_optimise_away(hmm);
 }
 
+TEST(hmm, constructor, arguments) {
+    gtl::hmm<float> hmm = gtl::hmm<float>(10, 100);
+    testbench::do_not_optimise_away(hmm);
+}
+
+TEST(hmm, function, train) {
+    gtl::hmm<float> hmm_1 = gtl::hmm<float>(2, 2);
+    unsigned long long int iterations_1 = hmm_1.train({
+        { 0, 1, 0 },
+        { 0, 1 },
+        { 0, 1, 0, 1, 0 },
+        { 1, 0, 1, 0 }
+    }, 1000, 1);
+    REQUIRE(iterations_1 == 1);
+    gtl::hmm<float> hmm_11 = gtl::hmm<float>(2, 2);
+    unsigned long long int iterations_11 = hmm_11.train({
+        { 0, 1, 0 },
+        { 0, 1 },
+        { 0, 1, 0, 1, 0 },
+        { 1, 0, 1, 0 }
+    }, 1000);
+    REQUIRE(iterations_11 == 11);
+}
+
+TEST(hmm, function, process) {
+    gtl::hmm<float> hmm = gtl::hmm<float>(2, 2);
+    hmm.train({
+        { 0, 1, 0 },
+        { 0, 1 },
+        { 0, 1, 0, 1, 0 },
+        { 1, 0, 1, 0 }
+    }, 1000);
+    float log_probability = hmm.process({1, 0, 1});
+    REQUIRE(log_probability > -2);
+}
+
+TEST(hmm, function, predict) {
+    gtl::hmm<float> hmm = gtl::hmm<float>(2, 2);
+    hmm.train({
+        { 0, 1, 0 },
+        { 0, 1 },
+        { 0, 1, 0, 1, 0 },
+        { 1, 0, 1, 0 }
+    }, 1000);
+    unsigned long long int next_zero = hmm.predict({ 0, 1, 0, 1 });
+    REQUIRE(next_zero == 0);
+    unsigned long long int next_one = hmm.predict({ 1, 0 });
+    REQUIRE(next_one == 1);
+}
+
+TEST(hmm, evaluate, ascending) {
+    gtl::hmm<float> hmm = gtl::hmm<float>(10, 10);
+
+    // A number of valid sequences, (notice all are ascending).
+    // These are designed so every ascending transition is covered.
+    std::vector<std::vector<unsigned long long int>> observations_training = {
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+        { 0, 2, 4, 6, 8 }, { 1, 3, 5, 7, 9 },
+        { 0, 3, 6, 9 }, { 1, 4, 7 }, { 2, 5, 8 },
+        { 0, 4, 8 }, { 1, 5, 9 }, { 2, 6 }, { 3, 7 }, { 4, 8 },
+        { 0, 5 }, { 1, 6 }, { 2, 7 }, { 3, 8 }, { 4, 9 },
+        { 0, 6 }, { 1, 7 }, { 2, 8 }, { 3, 9 },
+        { 0, 7 }, { 1, 8 }, { 2, 9 },
+        { 0, 8 }, { 1, 9 },
+        { 0, 9 },
+        { 0 }, { 1 }, { 2 }, { 3 }, { 4 }, { 5 }, { 6 }, { 7 }, { 8 }, { 9 }
+    };
+
+    // A number of valid sequences, (notice all are ascending).
+    std::vector<std::vector<unsigned long long int>> observations_valid = {
+        { 0, 1, 2, 8 },
+        { 0, 5, 6, 7 },
+        { 5 }
+    };
+
+    // A number of invalid sequences, (notice none are perfectly ascending).
+    std::vector<std::vector<unsigned long long int>> observations_invalid = {
+        { 0, 1, 2, 3, 4, 0, 6, 7, 8, 9 },
+        { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 },
+        { 8, 6, 4, 2, 0 },
+        { 0, 2, 1 },
+        { 0, 2, 4, 3, 2, 1 },
+        { 0, 4, 2, 1 },
+        { 1, 2, 5, 7, 3 },
+        { 0, 0, 0, 0 }
+    };
+
+    unsigned long long int iterations = hmm.train(observations_training);
+    PRINT("Iterations: %llu\n", iterations);
+
+    for (auto training : observations_training) {
+        PRINT("Training sequence: log(%f)\n", static_cast<double>(hmm.process(training)));
+        REQUIRE(hmm.process(training) > -15, "Probablity of training sequence too low: log(%f)\n", static_cast<double>(hmm.process(training)));
+    }
+    for (auto valid : observations_valid) {
+        PRINT("Valid sequence: log(%f)\n", static_cast<double>(hmm.process(valid)));
+        REQUIRE(hmm.process(valid) > -30, "Probablity of valid sequence too low: log(%f)\n", static_cast<double>(hmm.process(valid)));
+    }
+    for (auto invalid : observations_invalid) {
+        PRINT("Invalid sequence: log(%f)\n", static_cast<double>(hmm.process(invalid)));
+        REQUIRE(hmm.process(invalid) < -80, "Probablity of invalid sequence too high: log(%f)\n", static_cast<double>(hmm.process(invalid)));
+    }
+}
+
+TEST(hmm, evaluate, multiple_models) {
+    gtl::hmm<float> hmm_zeros = gtl::hmm<float>(2, 1);
+    gtl::hmm<float> hmm_ones = gtl::hmm<float>(2, 1);
+
+    hmm_zeros.train({
+        { 0 },
+        { 0, 0 },
+        { 0, 0, 0 }
+    });
+
+    hmm_ones.train({
+        { 1 },
+        { 1, 1 },
+        { 1, 1, 1 }
+    });
+
+    std::vector<unsigned long long int> test_zeros = { 0, 0, 0, 0 };
+    REQUIRE(hmm_zeros.process(test_zeros) > hmm_ones.process(test_zeros));
+
+    std::vector<unsigned long long int> test_ones = { 1, 1, 1, 1 };
+    REQUIRE(hmm_zeros.process(test_ones) < hmm_ones.process(test_ones));
+}
