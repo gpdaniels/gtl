30ef14ab007ae3006781142d0131be4de9189b90
Geoffrey Daniels <geoff@gpdaniels.com>
2020-07-08 17:20:29 +0100

    Adding static_string_encryption.

---
+++ Tests/string/static_string_encrypted.test.cpp
@@ -0,0 +1,147 @@
+/*
+The MIT License
+Copyright (c) 2019 Geoffrey Daniels. http://gpdaniels.com/
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE
+*/
+
+#include <main.tests.hpp>
+#include <comparison.tests.hpp>
+#include <optimise.tests.hpp>
+#include <process.tests.hpp>
+#include <require.tests.hpp>
+
+#include <string/static_string_encrypted>
+
+#if defined(_MSC_VER)
+#   pragma warning(push, 0)
+#endif
+
+#include <cstdio>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#if defined(_MSC_VER)
+#   pragma warning(pop)
+#endif
+
+TEST(static_string_encrypted, evaluate, obfusocated) {
+    REQUIRE(testbench::is_memory_same("STRING", GTL_STATIC_STRING_ENCRYPTED("STRING").get_decoded(), testbench::string_length("STRING")) == true);
+    REQUIRE(testbench::is_memory_same("STRING", GTL_STATIC_STRING_ENCRYPTED("STRING").get_encoded(), testbench::string_length("STRING")) == false);
+}
+
+TEST(static_string_encrypted, evaluate, executable_search) {
+
+    // Load the executable binary into memory.
+    const char* executable_path = testbench::get_executable();
+    REQUIRE(executable_path != nullptr);
+
+    std::FILE* executable_handle = std::fopen(executable_path, "rb");
+    REQUIRE(executable_handle != nullptr);
+
+    REQUIRE(std::fseek(executable_handle, 0, SEEK_END) == 0);
+    const long long int executable_length_and_return_code = std::ftell(executable_handle);
+    REQUIRE(executable_length_and_return_code > 0);
+    const unsigned long long int executable_length = static_cast<unsigned long long int>(executable_length_and_return_code);
+    REQUIRE(std::fseek(executable_handle, 0, SEEK_SET) == 0);
+
+    std::vector<char> executable_buffer(executable_length);
+
+    REQUIRE(std::fread(executable_buffer.data(), 1, executable_length, executable_handle) == executable_length);
+    std::fclose(executable_handle);
+
+    // Define some strings.
+    const char* secret_strings[] = {
+        GTL_STATIC_STRING_ENCRYPTED("static_string_encrypted::evaluate::executable_search = secret").get_decoded(),
+        GTL_STATIC_STRING_ENCRYPTED("static_string_encrypted::evaluate::executable_search = SECRET").get_decoded(),
+        GTL_STATIC_STRING_ENCRYPTED("static_string_encrypted::evaluate::executable_search = Hello secret world").get_decoded(),
+        GTL_STATIC_STRING_ENCRYPTED("static_string_encrypted::evaluate::executable_search = Some secret API function name").get_decoded()
+    };
+    testbench::do_not_optimise_away(secret_strings);
+    testbench::do_not_optimise_away(secret_strings[0]);
+    testbench::do_not_optimise_away(secret_strings[1]);
+    testbench::do_not_optimise_away(secret_strings[2]);
+    testbench::do_not_optimise_away(secret_strings[3]);
+
+    const char* public_strings[] = {
+        "static_string_encrypted::evaluate::executable_search = public",
+        "static_string_encrypted::evaluate::executable_search = PUBLIC",
+        "static_string_encrypted::evaluate::executable_search = Hello public world",
+        "static_string_encrypted::evaluate::executable_search = Some public API function name"
+    };
+    testbench::do_not_optimise_away(public_strings);
+    testbench::do_not_optimise_away(public_strings[0]);
+    testbench::do_not_optimise_away(public_strings[1]);
+    testbench::do_not_optimise_away(public_strings[2]);
+    testbench::do_not_optimise_away(public_strings[3]);
+
+    // Search the memory for the secret and non-secret strings.
+    auto search = [](const std::string& string, const std::vector<char>& buffer) -> unsigned long long int {
+        if (string.empty()) {
+            return false;
+        }
+        unsigned long long int found = 0;
+        for (unsigned long long int index_buffer = 0; index_buffer < buffer.size(); ++index_buffer) {
+            bool match = true;
+            for (unsigned long long int index_string = 0; index_string < string.size(); ++index_string) {
+                if (buffer[index_buffer + index_string] != string[index_string]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                ++found;
+            }
+        }
+        return found;
+    };
+
+    REQUIRE(search(secret_strings[0], executable_buffer) == 0);
+    REQUIRE(search(secret_strings[1], executable_buffer) == 0);
+    REQUIRE(search(secret_strings[2], executable_buffer) == 0);
+    REQUIRE(search(secret_strings[3], executable_buffer) == 0);
+
+    REQUIRE(search(public_strings[0], executable_buffer) == 1);
+    REQUIRE(search(public_strings[1], executable_buffer) == 1);
+    REQUIRE(search(public_strings[2], executable_buffer) == 1);
+    REQUIRE(search(public_strings[3], executable_buffer) == 1);
+}
+
+TEST(static_string_encrypted, evaluate, print) {
+    PRINT("Visible: '%s' vs. Hidden: '%s'\n", "Hello public world", GTL_STATIC_STRING_ENCRYPTED("Hello secret world").get_decoded());
+
+    const char* raw_string = "raw string";
+    unsigned long long int raw_string_length = testbench::string_length(raw_string);
+
+    PRINT("Raw:     ");
+    for (unsigned long long int i = 0; i < raw_string_length; ++i) {
+        PRINT("%02x", raw_string[i]);
+    }
+    PRINT("\n");
+
+    PRINT("Encoded: ");
+    for (unsigned long long int i = 0; i < raw_string_length; ++i) {
+        PRINT("%02x", GTL_STATIC_STRING_ENCRYPTED("raw string").get_encoded()[i]);
+    }
+    PRINT("\n");
+
+    PRINT("Decoded: ");
+    for (unsigned long long int i = 0; i < raw_string_length; ++i) {
+        PRINT("%02x", GTL_STATIC_STRING_ENCRYPTED("raw string").get_decoded()[i]);
+    }
+    PRINT("\n");
+}
