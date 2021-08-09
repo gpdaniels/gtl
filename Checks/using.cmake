51ae5c846e5a85ab3437fcd34234364dfb517e05
Geoffrey Daniels <geoff@gpdaniels.com>
2021-08-09 12:43:22 +0100

    Minor comment fix.

--- Checks/using.cmake
+++ Checks/using.cmake
@@ -42,7 +42,7 @@ FOREACH(SOURCE_FILE ${SOURCE_FILES})
     # Replace special list chars.
     STRING(REGEX REPLACE "([[]|[]])" "\\\\1" SOURCE_FILE_CONTENT "${SOURCE_FILE_CONTENT}")
     
-    # If there is no class definition then error.
+    # If 'using namespace' is detected raise an error.
     IF(SOURCE_FILE_CONTENT MATCHES "using namespace")
         MESSAGE(FATAL_ERROR "Detected 'using namespace' in ${SOURCE_FILE}.")
     ENDIF()
