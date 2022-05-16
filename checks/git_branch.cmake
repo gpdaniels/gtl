4d8b71d0c9206685927211f41a50c127a321a87d
Geoffrey Daniels <geoff@gpdaniels.com>
2022-05-16 08:59:52 +0100

    Fixing git branch check for shallow clones.

--- checks/git_branch.cmake
+++ checks/git_branch.cmake
@@ -45,7 +45,7 @@ IF(RETURN_CODE)
 ENDIF(RETURN_CODE)
 
 # Check the git branch matches one of the valid patterns.
-IF(NOT "${COMMIT_BRANCH}" MATCHES "^(master)|(dev)|(feature/.+)|(bug-fix/.+)$")
+IF(NOT "${COMMIT_BRANCH}" MATCHES "^master$|^dev$|^feature/.+$|^bug-fix/.+$|^HEAD$")
     MESSAGE(FATAL_ERROR "The current branch '${COMMIT_BRANCH}' is not valid.")
 ENDIF()
         
