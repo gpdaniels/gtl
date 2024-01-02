ae8238878deae2ea383b7c70e02f1299d610ef57
Geoffrey Daniels <geoff@gpdaniels.com>
2024-01-02 15:39:03 +0000

    Correcting variable names in access test to not use double underscores.

--- tests/debug/access.test.cpp
+++ tests/debug/access.test.cpp
@@ -32,95 +32,95 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 class test_class final {
 private:
     /*         storage */ /*volatile*/ /*const*/ /*type*/ /*name*/
-    /*            auto */ /*volatile*/ /*const*/    int   variable__a__i = 1;
-    /*            auto */   volatile   /*const*/    int   variable__av_i = 1;
-    /*            auto */ /*volatile*/   const      int   variable__a_ci = 1;
-    /*            auto */   volatile     const      int   variable__avci = 1;
-         inline static    /*volatile*/ /*const*/    int   variable__s__i = 1;
-         inline static      volatile   /*const*/    int   variable__sv_i = 1;
-         inline static    /*volatile*/   const      int   variable__s_ci = 1;
-         inline static      volatile     const      int   variable__svci = 1;
-               mutable    /*volatile*/ /*const*/    int   variable__m__i = 1;
-               mutable      volatile   /*const*/    int   variable__mv_i = 1;
-//             mutable    /*volatile*/   const      int   variable__m_ci = 1;
-//             mutable      volatile     const      int   variable__mvci = 1;
-      constexpr static    /*volatile*/ /*const*/    int   variable_xs__i = 1;
-      constexpr static      volatile   /*const*/    int   variable_xsv_i = 1;
-      constexpr static    /*volatile*/   const      int   variable_xs_ci = 1;
+    /*            auto */ /*volatile*/ /*const*/    int   variable_XaXXi = 1;
+    /*            auto */   volatile   /*const*/    int   variable_XavXi = 1;
+    /*            auto */ /*volatile*/   const      int   variable_XaXci = 1;
+    /*            auto */   volatile     const      int   variable_Xavci = 1;
+         inline static    /*volatile*/ /*const*/    int   variable_XsXXi = 1;
+         inline static      volatile   /*const*/    int   variable_XsvXi = 1;
+         inline static    /*volatile*/   const      int   variable_XsXci = 1;
+         inline static      volatile     const      int   variable_Xsvci = 1;
+               mutable    /*volatile*/ /*const*/    int   variable_XmXXi = 1;
+               mutable      volatile   /*const*/    int   variable_XmvXi = 1;
+//             mutable    /*volatile*/   const      int   variable_XmXci = 1;
+//             mutable      volatile     const      int   variable_Xmvci = 1;
+      constexpr static    /*volatile*/ /*const*/    int   variable_xsXXi = 1;
+      constexpr static      volatile   /*const*/    int   variable_xsvXi = 1;
+      constexpr static    /*volatile*/   const      int   variable_xsXci = 1;
       constexpr static      volatile     const      int   variable_xsvci = 1;
 
-      void function_a___() {}
-      int  function_a__i(/*volatile*/ /*const*/ int) { return 1; }
-      int  function_av_i(  volatile   /*const*/ int) { return 1; }
-      int  function_a_ci(/*volatile*/   const   int) { return 1; }
+      void function_aXXX() {}
+      int  function_aXXi(/*volatile*/ /*const*/ int) { return 1; }
+      int  function_avXi(  volatile   /*const*/ int) { return 1; }
+      int  function_aXci(/*volatile*/   const   int) { return 1; }
       int  function_avci(  volatile     const   int) { return 1; }
 
-      void function_c___() const {}
-      int  function_c__i(/*volatile*/ /*const*/ int) const { return 1; }
-      int  function_cv_i(  volatile   /*const*/ int) const { return 1; }
-      int  function_c_ci(/*volatile*/   const   int) const { return 1; }
+      void function_cXXX() const {}
+      int  function_cXXi(/*volatile*/ /*const*/ int) const { return 1; }
+      int  function_cvXi(  volatile   /*const*/ int) const { return 1; }
+      int  function_cXci(/*volatile*/   const   int) const { return 1; }
       int  function_cvci(  volatile     const   int) const { return 1; }
 
-      static void function_s___() {}
-      static int  function_s__i(/*volatile*/ /*const*/ int) { return 1; }
-      static int  function_sv_i(  volatile   /*const*/ int) { return 1; }
-      static int  function_s_ci(/*volatile*/   const   int) { return 1; }
+      static void function_sXXX() {}
+      static int  function_sXXi(/*volatile*/ /*const*/ int) { return 1; }
+      static int  function_svXi(  volatile   /*const*/ int) { return 1; }
+      static int  function_sXci(/*volatile*/   const   int) { return 1; }
       static int  function_svci(  volatile     const   int) { return 1; }
 
 public:
       int use() {
           return 0
-             + this->variable__a__i
-             + this->variable__av_i
-             + this->variable__a_ci
-             + this->variable__avci
-             + test_class::variable__s__i
-             + test_class::variable__sv_i
-             + test_class::variable__s_ci
-             + test_class::variable__svci
-             + this->variable__m__i
-             + this->variable__mv_i
-//           + this->variable__m_ci
-//           + this->variable__mvci
-             + test_class::variable_xs__i
-             + test_class::variable_xsv_i
-             + test_class::variable_xs_ci
+             + this->variable_XaXXi
+             + this->variable_XavXi
+             + this->variable_XaXci
+             + this->variable_Xavci
+             + test_class::variable_XsXXi
+             + test_class::variable_XsvXi
+             + test_class::variable_XsXci
+             + test_class::variable_Xsvci
+             + this->variable_XmXXi
+             + this->variable_XmvXi
+//           + this->variable_XmXci
+//           + this->variable_Xmvci
+             + test_class::variable_xsXXi
+             + test_class::variable_xsvXi
+             + test_class::variable_xsXci
              + test_class::variable_xsvci;
       }
 };
 
 GTL_ACCESS_GENERATE(test_class_access, test_class,
-    GTL_ACCESS_TYPE,    auto,                int, variable__a__i,
-    GTL_ACCESS_TYPE,    auto,       volatile int, variable__av_i,
-    GTL_ACCESS_TYPE,    auto,          const int, variable__a_ci,
-    GTL_ACCESS_TYPE,    auto, volatile const int, variable__avci,
-    GTL_ACCESS_TYPE,  static,                int, variable__s__i,
-    GTL_ACCESS_TYPE,  static,       volatile int, variable__sv_i,
-    GTL_ACCESS_TYPE,  static,          const int, variable__s_ci,
-    GTL_ACCESS_TYPE,  static, volatile const int, variable__svci,
-    GTL_ACCESS_TYPE, mutable,                int, variable__m__i,
-    GTL_ACCESS_TYPE, mutable,       volatile int, variable__mv_i,
-//  GTL_ACCESS_TYPE, mutable,          const int, variable__m_ci,
-//  GTL_ACCESS_TYPE, mutable, volatile const int, variable__mvci,
-    GTL_ACCESS_TYPE,  static,          const int, variable_xs__i,
-    GTL_ACCESS_TYPE,  static, volatile const int, variable_xsv_i,
-    GTL_ACCESS_TYPE,  static,          const int, variable_xs_ci,
+    GTL_ACCESS_TYPE,    auto,                int, variable_XaXXi,
+    GTL_ACCESS_TYPE,    auto,       volatile int, variable_XavXi,
+    GTL_ACCESS_TYPE,    auto,          const int, variable_XaXci,
+    GTL_ACCESS_TYPE,    auto, volatile const int, variable_Xavci,
+    GTL_ACCESS_TYPE,  static,                int, variable_XsXXi,
+    GTL_ACCESS_TYPE,  static,       volatile int, variable_XsvXi,
+    GTL_ACCESS_TYPE,  static,          const int, variable_XsXci,
+    GTL_ACCESS_TYPE,  static, volatile const int, variable_Xsvci,
+    GTL_ACCESS_TYPE, mutable,                int, variable_XmXXi,
+    GTL_ACCESS_TYPE, mutable,       volatile int, variable_XmvXi,
+//  GTL_ACCESS_TYPE, mutable,          const int, variable_XmXci,
+//  GTL_ACCESS_TYPE, mutable, volatile const int, variable_Xmvci,
+    GTL_ACCESS_TYPE,  static,          const int, variable_xsXXi,
+    GTL_ACCESS_TYPE,  static, volatile const int, variable_xsvXi,
+    GTL_ACCESS_TYPE,  static,          const int, variable_xsXci,
     GTL_ACCESS_TYPE,  static, volatile const int, variable_xsvci,
 
-    GTL_ACCESS_FUNC,    auto, void(void),                    function_a___,
-    GTL_ACCESS_FUNC,    auto, int(int),                      function_a__i,
-    GTL_ACCESS_FUNC,    auto, int(volatile int),             function_av_i,
-    GTL_ACCESS_FUNC,    auto, int(const int),                function_a_ci,
+    GTL_ACCESS_FUNC,    auto, void(void),                    function_aXXX,
+    GTL_ACCESS_FUNC,    auto, int(int),                      function_aXXi,
+    GTL_ACCESS_FUNC,    auto, int(volatile int),             function_avXi,
+    GTL_ACCESS_FUNC,    auto, int(const int),                function_aXci,
     GTL_ACCESS_FUNC,    auto, int(volatile const int),       function_avci,
-    GTL_ACCESS_FUNC,    auto, void(void) const,              function_c___,
-    GTL_ACCESS_FUNC,    auto, int(int) const,                function_c__i,
-    GTL_ACCESS_FUNC,    auto, int(volatile int) const,       function_cv_i,
-    GTL_ACCESS_FUNC,    auto, int(const int) const,          function_c_ci,
+    GTL_ACCESS_FUNC,    auto, void(void) const,              function_cXXX,
+    GTL_ACCESS_FUNC,    auto, int(int) const,                function_cXXi,
+    GTL_ACCESS_FUNC,    auto, int(volatile int) const,       function_cvXi,
+    GTL_ACCESS_FUNC,    auto, int(const int) const,          function_cXci,
     GTL_ACCESS_FUNC,    auto, int(volatile const int) const, function_cvci,
-    GTL_ACCESS_FUNC,  static, void(void),                    function_s___,
-    GTL_ACCESS_FUNC,  static, int(int),                      function_s__i,
-    GTL_ACCESS_FUNC,  static, int(volatile int),             function_sv_i,
-    GTL_ACCESS_FUNC,  static, int(const int),                function_s_ci,
+    GTL_ACCESS_FUNC,  static, void(void),                    function_sXXX,
+    GTL_ACCESS_FUNC,  static, int(int),                      function_sXXi,
+    GTL_ACCESS_FUNC,  static, int(volatile int),             function_svXi,
+    GTL_ACCESS_FUNC,  static, int(const int),                function_sXci,
     GTL_ACCESS_FUNC,  static, int(volatile const int),       function_svci
 );
 
@@ -152,36 +152,36 @@ TEST(access, evaluate, access) {
     test_class test;
     test_class_access access(test);
 
-    REQUIRE(access.variable__a__i == 1);
-    REQUIRE(access.variable__av_i == 1);
-    REQUIRE(access.variable__a_ci == 1);
-    REQUIRE(access.variable__avci == 1);
-    REQUIRE(access.variable__s__i == 1);
-    REQUIRE(access.variable__sv_i == 1);
-    REQUIRE(access.variable__s_ci == 1);
-    REQUIRE(access.variable__svci == 1);
-    REQUIRE(access.variable__m__i == 1);
-    REQUIRE(access.variable__mv_i == 1);
-//  REQUIRE(access.variable__m_ci == 1);
-//  REQUIRE(access.variable__mvci == 1);
-    REQUIRE(access.variable_xs__i == 1);
-    REQUIRE(access.variable_xsv_i == 1);
-    REQUIRE(access.variable_xs_ci == 1);
+    REQUIRE(access.variable_XaXXi == 1);
+    REQUIRE(access.variable_XavXi == 1);
+    REQUIRE(access.variable_XaXci == 1);
+    REQUIRE(access.variable_Xavci == 1);
+    REQUIRE(access.variable_XsXXi == 1);
+    REQUIRE(access.variable_XsvXi == 1);
+    REQUIRE(access.variable_XsXci == 1);
+    REQUIRE(access.variable_Xsvci == 1);
+    REQUIRE(access.variable_XmXXi == 1);
+    REQUIRE(access.variable_XmvXi == 1);
+//  REQUIRE(access.variable_XmXci == 1);
+//  REQUIRE(access.variable_Xmvci == 1);
+    REQUIRE(access.variable_xsXXi == 1);
+    REQUIRE(access.variable_xsvXi == 1);
+    REQUIRE(access.variable_xsXci == 1);
     REQUIRE(access.variable_xsvci == 1);
 
-    access.function_a___();
-    REQUIRE(access.function_a__i(1) == 1);
-    REQUIRE(access.function_av_i(1) == 1);
-    REQUIRE(access.function_a_ci(1) == 1);
+    access.function_aXXX();
+    REQUIRE(access.function_aXXi(1) == 1);
+    REQUIRE(access.function_avXi(1) == 1);
+    REQUIRE(access.function_aXci(1) == 1);
     REQUIRE(access.function_avci(1) == 1);
-    access.function_c___();
-    REQUIRE(access.function_c__i(1) == 1);
-    REQUIRE(access.function_cv_i(1) == 1);
-    REQUIRE(access.function_c_ci(1) == 1);
+    access.function_cXXX();
+    REQUIRE(access.function_cXXi(1) == 1);
+    REQUIRE(access.function_cvXi(1) == 1);
+    REQUIRE(access.function_cXci(1) == 1);
     REQUIRE(access.function_cvci(1) == 1);
-    access.function_s___();
-    REQUIRE(access.function_s__i(1) == 1);
-    REQUIRE(access.function_sv_i(1) == 1);
-    REQUIRE(access.function_s_ci(1) == 1);
+    access.function_sXXX();
+    REQUIRE(access.function_sXXi(1) == 1);
+    REQUIRE(access.function_svXi(1) == 1);
+    REQUIRE(access.function_sXci(1) == 1);
     REQUIRE(access.function_svci(1) == 1);
 }
