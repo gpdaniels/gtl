dc2f62ed8f82a52360642deedbeee508af46be3c
Geoffrey Daniels <geoff@gpdaniels.com>
2024-07-17 21:09:33 +0100

    Add a simple match type and bruteforce matcher.

---
+++ tests/vision/match/match.test.cpp
@@ -0,0 +1,29 @@
+/*
+Copyright (C) 2018-2024 Geoffrey Daniels. https://gpdaniels.com/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License only.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <main.tests.hpp>
+#include <optimise.tests.hpp>
+#include <require.tests.hpp>
+
+#include <vision/match/match>
+
+TEST(match, constructor, empty) {
+    gtl::match match;
+    testbench::do_not_optimise_away(match);
+}
+
+// TODO: Implement tests for match.
+
