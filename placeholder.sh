11def98f3684d70b646bc094e94a098c047b6220
Geoffrey Daniels <geoff@gpdaniels.com>
2025-07-16 16:11:27 +0100

    Fix placeholder script.

--- placeholder.sh
+++ placeholder.sh
@@ -75,9 +75,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <main.tests.hpp>
-#include <optimise.tests.hpp>
-#include <require.tests.hpp>
+#include <testbench/main.tests.hpp>
+
+#include <testbench/optimise.tests.hpp>
+#include <testbench/require.tests.hpp>
 
 #include <${TYPE_NAME_LOWER}/${CLASS_NAME_LOWER}>
 
